87bc7a03a7fc89091fd9e7c85a9e2a10663e98d3
Guilherme da Silva Boava <guiboava@hotmail.com>
2025-08-13 11:28:42 -0300

    Initialized project

---
+++ src/test/java/io/github/guiboava/bem_dosado/BemDosadoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.guiboava.bem_dosado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemDosadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
